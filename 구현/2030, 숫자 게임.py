d90bf7161a2492099cdb3a662cf2eb0dd1e163b6
jin-develop <nick02190219@gmail.com>
2021-04-22 19:51:16 +0900

    ..

--- 구현/2030, 숫자 게임.py
+++ 구현/2030, 숫자 게임.py
@@ -1,19 +1,18 @@
 T = int(input())
-num_org = []
-for w in range(T):
-    array = list(map(int,input().split()))
-    result1 = [] 
-    for i in range(len(array)):
-        for j in range(i, len(array)):
-            for k in range(j, len(array)):
-                result = array[i] + array[j] + array[k]
-                result1.append(result)
-        
-    for z in range(len(result1)):
-        
-    num = max(result1)
-    num_org.append(num)
-    num1 = num[-1]
 
+num = [] 
+for test in range(T):
+    array = list(map(int,input().split()))
+    result = 0
+    arr = []
+    for i in range(len(array)):
+        for j in range(i+1, len(array)):
+            for k in range(j+1, len(array)):
+                result =str(array[i] + array[j] + array[k])
+                arr.append(result)
+
+    print(arr)
+
+        
 
 

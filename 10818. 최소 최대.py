468cf31ecefc00e0d4a63f4204d7bfb730c60cc4
jin-develop <nick02190219@gmail.com>
2021-06-09 15:22:54 +0900

    go

---
+++ 10818. 최소 최대.py
@@ -0,0 +1,5 @@
+N = int(input())
+
+array = list(map(int,input().split()))
+
+print(min(array), max(array))
--- 팩토리얼.py
+++ 팩토리얼.py
@@ -4,8 +4,19 @@ n = int(input())
 def fac(n):
     if n==0:
         return 0
-    if n == 1:
+    elif n == 1:
         return 1
     return n * fac(n-1)
 
 print(fac(n))
+
+N = int(input())
+ 
+def factorial(N):
+    if N == 0:
+        return 1
+    elif N == 1:
+        return 1
+    return N * factorial(N-1)
+ 
+print(factorial(N))
